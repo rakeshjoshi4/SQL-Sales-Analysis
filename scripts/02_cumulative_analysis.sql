7035733b3123b3fe77fd045694bfab473660df0c
Rakesh Joshi <rakeshgjoshi4@gmail.com>
2025-04-12 22:05:48 +0530

    Update 02_cumulative_analysis.sql

--- scripts/02_cumulative_analysis.sql
+++ scripts/02_cumulative_analysis.sql
@@ -4,14 +4,16 @@
 SELECT 
 	order_date,
 	total_sales,
-	SUM(total_sales) OVER (ORDER BY order_date) AS running_total_sales,
-	AVG(avg_price) OVER (ORDER BY order_date) AS moving_price_total
+	running_total_sales,
+	moving_price_total
 FROM
 (
 	SELECT 
 		DATETRUNC(MONTH, order_date) AS order_date,
 		SUM(sales_amount) AS total_sales,
-		AVG(price) AS avg_price
+		AVG(price) AS avg_price,
+		SUM(SUM(sales_amount)) OVER (ORDER BY DATETRUNC(MONTH, order_date)) AS running_total_sales,
+		AVG(AVG(price)) OVER (ORDER BY DATETRUNC(MONTH, order_date)) AS moving_avg_price
 	FROM gold.fact_sales
 	WHERE order_date IS NOT NULL
 	GROUP BY DATETRUNC(MONTH, order_date)
@@ -22,14 +24,16 @@ FROM
 SELECT 
 	order_date,
 	total_sales,
-	SUM(total_sales) OVER (ORDER BY order_date) AS running_total_sales,
-	AVG(avg_price) OVER (ORDER BY order_date) AS moving_price_total
+	running_total_sales,
+	moving_price_total
 FROM
 (
 	SELECT 
 		DATETRUNC(YEAR, order_date) AS order_date,
 		SUM(sales_amount) AS total_sales,
 		AVG(price) AS avg_price
+		SUM(SUM(sales_amount)) OVER (ORDER BY DATETRUNC(YEAR, order_date)) AS running_total_sales,
+		AVG(AVG(price)) OVER (ORDER BY DATETRUNC(YEAR, order_date)) AS moving_avg_price
 	FROM gold.fact_sales
 	WHERE order_date IS NOT NULL
 	GROUP BY DATETRUNC(YEAR, order_date)
